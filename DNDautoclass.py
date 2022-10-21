ea57d8b3e6fe67e6ad5cbf45b0edd5ac98f7bb19
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2022-10-21 14:41:09 -0500

    Add files via upload

--- DNDautoclass.py
+++ DNDautoclass.py
@@ -1,5 +1,20 @@
-from .DNDoneshot
+import DNDoneshot
 
-character = name,age,weapon,gender,race
+Class Character:
+    def __init__(name,age,weapon,gender,race):
+        self.name = name
+        self.age = age
+        self.weapon = weapon
+        self.gender = gender
+        self.race = race
+    
+    def swing_weapon(self):
+        print("You swing your weapon")
+    
+    def getinfo(self):
+        return {name: self.name,
+                age: self.age,
+                weapon: self.weapon,
+                gender: self.gender,
+                race: self.race}
 
-def character:
